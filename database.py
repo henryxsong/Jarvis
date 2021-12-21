de1c383b76d3f4de58d9edb7cb26552c23e37193
Henry Song <henry.song671@gmail.com>
2021-12-21 22:23:00 +1000

    added performing task replies in database.py

---
+++ database.py
@@ -0,0 +1,14 @@
+performing_task = [
+    "working on it",
+    "right away",
+    "running task",
+    "doing it",
+    "on it",
+    "engaging nuetron torpedoes",
+    "moving Mount Everest",
+    "engaging the Death Star",
+    "attempting to contact the Dark Side",
+    "attempting",
+    "starting",
+    "launching",
+]
