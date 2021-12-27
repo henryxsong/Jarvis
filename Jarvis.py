2abaf768737cde01ebe63d4124a05260745b21ad
Henry Song <henry.song671@gmail.com>
2021-12-27 18:50:20 +1000

    structured Jarvis.py into class

--- Jarvis.py
+++ Jarvis.py
@@ -2,7 +2,6 @@
 import pyttsx3
 import speech_recognition as sr
 from random import choice
-#from utils import opening_text
 from decouple import config
 from datetime import datetime
 from phrase_database import performing_task
@@ -10,70 +9,76 @@ from phrase_database import performing_task
 USERNAME = config('USER')
 BOTNAME = config('BOTNAME')
 
-engine = pyttsx3.init()
+class Engine(object):
+    def __init__(self) -> None:
+        super().__init__()
+        self.engine = pyttsx3.init()
 
-engine.setProperty('rate', 200)
-engine.setProperty('volume', 1)
+        self.engine.setProperty('rate', 205)
+        self.engine.setProperty('volume', 1)
 
-voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[7].id) #voices index 7 represesnts a british male voice, similar to JARVIS
+        self.voices = self.engine.getProperty('voices')
+        self.engine.setProperty('voice', self.voices[7].id) #voices index 7 represesnts a british male voice, similar to JARVIS
 
-def speak(txt:str) -> None:
-    '''
-    Synthesizes speech based on given text
+    def speak(self, txt:str) -> None:
+        '''
+        Synthesizes speech based on given text
 
-    :param txt: string of text to speechify
-    '''
-    engine.say(txt)
-    engine.runAndWait()
+        :param txt: string of text to speechify
+        '''
+        self.engine.say(txt)
+        self.engine.runAndWait()
 
-def greet_user() -> None:
-    """
-    Greets user according to time of day (morning, afternoon, evening)
-    """
-    current_time = datetime.now()
-    current_hour = current_time.hour
+    def greet_user(self) -> None:
+        """
+        Greets user according to time of day (morning, afternoon, evening)
+        """
+        current_time = datetime.now()
+        current_hour = current_time.hour
 
-    if current_hour >= 0 and current_hour < 12:
-        speak(f"Good morning {USERNAME}")
-    elif current_hour >= 12 and current_hour < 18:
-        speak(f"Good afternoon {USERNAME}")
-    elif current_hour >= 18 and current_hour < 24:
-        speak(f"Good evening {USERNAME}")
-    speak(f"I am {BOTNAME}. How may I assist you?")
+        if current_hour >= 0 and current_hour < 12:
+            self.speak(f"Good morning {USERNAME}")
+        elif current_hour >= 12 and current_hour < 18:
+            self.speak(f"Good afternoon {USERNAME}")
+        elif current_hour >= 18 and current_hour < 24:
+            self.speak(f"Good evening {USERNAME}")
+        self.speak(f"I am {BOTNAME}. How may I assist you?")
 
-def listen() -> str:
-    """Takes user input, recognizes it using Speech Recognition module and converts it into text"""
-    #TODO: add variation of responses
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
-        print('Listening....')
-        r.pause_threshold = 1
-        audio = r.listen(source)
+    def listen(self) -> str:
+        """Takes user input, recognizes it using Speech Recognition module and converts it into text"""
+        #TODO: add variation of responses
+        r = sr.Recognizer()
+        with sr.Microphone() as source:
+            print('Listening....')
+            r.pause_threshold = 1
+            audio = r.listen(source)
 
-    try:
-        print('Recognizing...')
-        query = r.recognize_google(audio, language='en-us')
-        if not 'exit' in query or 'stop' in query or 'quit' in query:
-            speak(query)
-            speak(choice(performing_task))
-        else:
-            hour = datetime.now().hour
-            if hour >= 21 and hour < 6:
-                speak("Good night sir, take care!")
+        try:
+            print('Recognizing...')
+            query = r.recognize_google(audio, language='en-us')
+            if not 'exit' in query or 'stop' in query or 'quit' in query:
+                self.speak(query)
+                self.speak(choice(performing_task))
             else:
-                speak('Have a good day sir!')
-            exit()
-    except Exception:
-        speak('Sorry, I could not understand. Could you please say that again?')
-        query = 'None'
-    return query
+                hour = datetime.now().hour
+                if hour >= 21 and hour < 6:
+                    self.speak("Good night sir, take care!")
+                else:
+                    self.speak('Have a good day sir!')
+                exit()
+        except Exception:
+            self.speak('Sorry, I could not understand. Could you please say that again?')
+            query = 'None'
+        return query
+
+
 
 def main():
-    speak("Initiating...")
-    greet_user()
+    jarvis = Engine()
+    jarvis.speak("Initiating...")
+    jarvis.greet_user()
     while True:
-        listen()
+        jarvis.listen()
 
 if __name__ == '__main__':
     main()
