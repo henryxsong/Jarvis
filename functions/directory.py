e0a059e1efbef3a0a9837b8c24a4e983bc77999a
Henry Song <henry.song671@gmail.com>
2022-01-06 15:50:27 -0800

    fixed path error in directory.py

--- functions/directory.py
+++ functions/directory.py
@@ -1,10 +1,15 @@
 #TODO: Create function to return all app paths into dictionary structure
 import os
+
+COMMAND = ["/usr/bin/open", "-n", "-a"]
+
 class Directory(object):
     def __init__(self) -> None:
         super().__init__()
         self.app_paths = []
         self.initialize()
+
+  
     
     def parse_app_name(self, app_name:str) -> str:
         """
@@ -12,7 +17,7 @@ class Directory(object):
         """
         return app_name[:-4]
         
-    def add_app_path(self, app_name:str, app_path:str) -> None:
+    def add_app_path(self, app_name:str, app_path:list) -> None:
         """
         Adds app path to dictionary
         """
@@ -22,12 +27,20 @@ class Directory(object):
         dir = "/Applications/"
         for path in os.listdir(dir):
             full_path = os.path.join(dir, path)
-            self.add_app_path(self.parse_app_name(path), full_path)
+            temp = COMMAND + [full_path]
+            self.add_app_path(self.parse_app_name(path), temp)
         print(self.app_paths)
 
+    @staticmethod
+    def get_app_paths() -> list:
+        """
+        Returns all app paths
+        """
+        return Directory.app_paths
 
 
-    COMMAND = ["/usr/bin/open", "-n", "-a"]
+
+    
 
 
 print(Directory())
