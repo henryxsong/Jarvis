a87820f120db122c975152cc60d1e17278d9e0f4
Henry Song <henry.song671@gmail.com>
2022-01-15 12:24:53 -0800

    added close_app method in ops.py

--- functions/ops.py
+++ functions/ops.py
@@ -1,22 +1,37 @@
-from os import startfile
+import os
 import subprocess as sp
-from directory import app_paths
+import time
+import directory
+import psutil
+
+from directory import Directory
 
 class Operations(object):
     def __init__(self) -> None:
         super().__init__()
+        self.app_dir = Directory().get_app_paths()
+        
     
-    @staticmethod
-    def open_app(app_name:str) -> None:
+    def open_app(self, app_name:str) -> None:
         """
         Opens an application based on given app name
         """
-        for app in app_paths:
-            if app_name in app:
-                startfile(app_paths[app])
+        for i in range(len(self.app_dir)):
+            if app_name in self.app_dir[i]:
+                sp.call(self.app_dir[i][app_name])
                 break
         else:
             print(f"{app_name} not found")
+    
 
-if __name__ == "__main__":
-    Operations.open_app("Discord")
+    def close_app(self, app_name:str) -> None:
+        """
+        Closes an application based on given app name
+        """
+        try:
+            for process in (process for process in psutil.process_iter() if process.name()==app_name):
+                process.kill()
+        except:
+            print(f"{app_name} process not open")
+
+
